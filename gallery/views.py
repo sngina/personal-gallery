690d2ebd6623f148db3bbe6b9451ac260156653f
sngina <sngina707@gmail.com>
2021-07-02 13:15:15 +0300

    installed psycopg2

--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def welcome(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
+    return HttpResponse('Welcome to my  gallery')
+

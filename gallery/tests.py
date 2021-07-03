48aaaee7d13273488022697907de05a12611ba37
sngina <sngina707@gmail.com>
2021-07-03 12:36:22 +0300

    test save method

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .models import Category , Image , Location
 
 # Create your tests here.
+class CategoryTestClass(TestCase):
+    #setup method
+    def setUp(self):
+        self.image = Category(selfies = 'Image')
+        #test instances 
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image , Category))
+
+    # test save method
+    def test_save_method(self):
+        self.image.save_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category) >0)
+
+        
+
+
+
